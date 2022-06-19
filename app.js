8e70ca3e08debc318aefbd221b83b31fdf19c034
Siddhartha G <-->
2022-06-19 16:12:27 +0530

    Blog ready to be deployed

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
+const atlasRemote = process.env.ATLAS_URL;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost:27017/blogs");
+mongoose.connect(atlasRemote);
 
 const blogSchema = mongoose.Schema(
   {
@@ -24,11 +28,33 @@ const blogSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const BlogPost = mongoose.model("blogposts", blogSchema);
+const BlogPost = mongoose.model("blogs", blogSchema);
 
 app.get("/", (req, res) => {
-  BlogPost.find((err, blogs) => {
+  BlogPost.find((err, blogsRes) => {
     if (!err) {
+      let blogs = [];
+
+      blogsRes.forEach((elmt, idx) => {
+        const { id, title, content, createdAt } = elmt;
+        let timestamp = new Date(createdAt);
+        let date =
+          timestamp.getDate() +
+          "-" +
+          (timestamp.getMonth() + 1) +
+          "-" +
+          timestamp.getFullYear();
+        let time = timestamp.getHours() + ":" + timestamp.getMinutes();
+
+        blogs.push({
+          id: id,
+          title: title,
+          content: content,
+          date: date,
+          time: time,
+        });
+      });
+      console.log(blogs);
       res.render("index.ejs", { blogs: blogs });
     } else {
       //Handle the error
@@ -58,9 +84,9 @@ app.post("/create", (req, res) => {
 });
 
 app.post("/delete", (req, res) => {
-  const { deleteTitle } = req.body;
+  const deleteID = mongoose.Types.ObjectId(req.body.deleteID);
 
-  BlogPost.remove({ title: deleteTitle }, (err, result) => {
+  BlogPost.deleteOne({ id: deleteID }, (err, result) => {
     if (!err) {
       res.redirect("/");
     } else {
@@ -71,20 +97,6 @@ app.post("/delete", (req, res) => {
   });
 });
 
-app.get("/:articleName", (req, res) => {
-  const articleName = req.params.articleName;
-
-  BlogPost.find({ title: articleName }, (err, result) => {
-    if (!err) {
-      res.send(result);
-    } else {
-      //Handle the error
-      console.log("Error:", err);
-      res.redirect("/oops");
-    }
-  });
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server Started on Port:3000");
 });
